a083596f6d4702037d45e7d38d4397f681a5b55f
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-06-24 10:30:16 +0900

    Font option

--- internal/hlopt/hlopt.go
+++ internal/hlopt/hlopt.go
@@ -10,3 +10,13 @@ type nopOption struct{}
 func (*nopOption) Apply(g *highlight.Group) {}
 
 var nop *nopOption
+
+// Font returns Option to apply a font to Font field of Group.
+func Font(font string) highlight.Option {
+	if font == "" {
+		return nop
+	}
+	return highlight.OptionFunc(func(g *highlight.Group) {
+		g.Font = font
+	})
+}
